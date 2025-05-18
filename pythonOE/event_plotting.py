3368c25829f7f17f1a5e4a796d030e420dcaf814
Marine Guyot <marine.chaput@hotmail.fr>
2025-05-18 16:55:24 +0200

    organize in class

--- pythonOE/event_plotting.py
+++ pythonOE/event_plotting.py
@@ -8,107 +8,97 @@ import asyncio
 from functools import partial
 from concurrent.futures import ThreadPoolExecutor
 
-global repaint
-repaint = False
 
-N_CHANNELS = 192
-doc = curdoc()
+class EventView:
+    def __init__(self, stream):
+        self.repaint = False
 
-sources = [ColumnDataSource(data=dict(x=[], y=[])) for _ in range(N_CHANNELS)]
-global event_type 
-event_type = "Average"
+        self.executor = ThreadPoolExecutor()
 
+        self.n_channels = 192
+        self.doc = curdoc()
 
-def update_type_event(attr, old, new):
-    global event_type, repaint
-    event_type = new
-    repaint = True
+        self.sources = [ColumnDataSource(data=dict(x=[], y=[])) for _ in range(self.n_channels)]
+        self.event_type = "Average"
 
-dropdown = Select(name = "Events:", value = "Average", options=["Average"])
-dropdown.on_change("value", update_type_event)
+        self.dropdown = Select(name = "Events:", value = "Average", options=["Average"])
+        self.dropdown.on_change("value", self.update_type_event)
+        
+        plots = []
+        for i in range(self.n_channels):
+            plot = figure(
+                height=150,
+                width=150,
+                tools="pan,wheel_zoom,reset",
+                toolbar_location=None,
+                title="C " + str((i%24)*4) + "-" + str((i%24+1)*4-1) + "\nL " + str(int(i/24)*4)+"-"+str(int(i/24+1)*4-1) ,
+                output_backend="webgl"
+            )
+            plot.line(x="x", y="y", source=self.sources[i])
+            plot.xaxis.visible = False
+            plot.yaxis.visible = False
+            plots.append(plot)
 
+        grid = gridplot(plots, ncols=24) #96
+        layout = column(self.dropdown, grid)
 
-plots = []
-for i in range(N_CHANNELS):
-    plot = figure(
-        height=150,
-        width=150,
-        tools="pan,wheel_zoom,reset",
-        toolbar_location=None,
-        title="C " + str((i%24)*4) + "-" + str((i%24+1)*4-1) + "\nL " + str(int(i/24)*4)+"-"+str(int(i/24+1)*4-1) ,
-        output_backend="webgl"
-    )
-    plot.line(x="x", y="y", source=sources[i])
-    plot.xaxis.visible = False
-    plot.yaxis.visible = False
-    plots.append(plot)
+        self.stream = stream
+        self.stream.start()
 
-# Grid layout (96 columns in your case)
-grid = gridplot(plots, ncols=24) #96
-layout = column(dropdown, grid)
+        self.x_vals = list(range(int(self.stream.event_snapshot_duration * self.stream.fs)*2))
 
-stream.start()
-
-# Schedule source update on main thread
-global x_vals
-x_vals = list(range(int(stream.event_snapshot_duration * stream.fs)*2))
+        self.doc.add_root(layout)
+        self.doc.add_periodic_callback(self.async_update_sources, 100)
+        self.doc.title = "Event live plotting"
 
 
-doc.add_root(layout)
-
-# Executor for async tasks
-executor = ThreadPoolExecutor()
-
-# Helper: fetch and average data for one source
-def compute_data(i, event_ts):
-    start = event_ts - int(stream.event_snapshot_duration * stream.fs)
-    end = min(event_ts + int(stream.event_snapshot_duration * stream.fs), len(stream.data))
-    data_slice = stream.data[start:end, int((i/24)*4):int((i/24+1)*4), int((i%24)*4):int((i%24+1)*4)]
-    
-    return np.mean(data_slice, axis=(1, 2)).tolist()
-
-def update_dropdown_options():
-    dropdown.options = ["Average"] + [str(ev) for ev in stream.events] 
+    def update_type_event(self, attr, old, new):
+        self.event_type = new
+        self.repaint = True
 
 
-# Async wrapper for non-blocking execution
-@without_document_lock
-async def async_update_sources():
-    global repaint, x_vals
-    if stream.repaint or repaint:
+    def compute_data(self, i, event_ts):
+        start = event_ts - int(self.stream.event_snapshot_duration * self.stream.fs)
+        end = min(event_ts + int(self.stream.event_snapshot_duration * self.stream.fs), len(self.stream.data))
+        data_slice = stream.data[start:end, int((i/24)*4):int((i/24+1)*4), int((i%24)*4):int((i%24+1)*4)]
+        
+        return np.mean(data_slice, axis=(1, 2)).tolist()
 
-        doc.add_next_tick_callback(update_dropdown_options)
-        stream.repaint= False
-        repaint = False
+    def update_dropdown_options(self):
+        self.dropdown.options = ["Average"] + [str(ev) for ev in self.stream.events] 
 
-        def compute_all_channels(event_ts):
-            return [compute_data(i, event_ts) for i in range(N_CHANNELS)]
+    def compute_all_channels(self, event_ts):
+        return [self.compute_data(i, event_ts) for i in range(self.n_channels)]
+        
+    # Async wrapper for non-blocking execution
+    @without_document_lock
+    async def async_update_sources(self):
 
-        if event_type == "Average":
-            # Gather all individual event data and average
+        if self.stream.repaint or self.repaint:
 
-            # Launch in background
-            all_data = await asyncio.gather(*[
-                asyncio.wrap_future(executor.submit(compute_all_channels, ts))
-                for ts in stream.events
-            ])
+            self.doc.add_next_tick_callback(self.update_dropdown_options)
+            self.stream.repaint= False
+            self.repaint = False
 
-            # Average across all events per channel
-            averaged = np.mean(all_data, axis=0)  # shape: [N_CHANNELS][T]
-            for i, source in enumerate(sources):
-                doc.add_next_tick_callback(partial(update_source, source, x_vals, averaged[i]))
-        else:
-            # Single event selected
-            event_ts = int(event_type)
-            one_data = await asyncio.wrap_future(executor.submit(compute_all_channels, event_ts))
+            if self.event_type == "Average":
 
-            for i, source in enumerate(sources):
-                doc.add_next_tick_callback(partial(update_source, source, x_vals, one_data[i]))
+                all_data = await asyncio.gather(*[
+                    asyncio.wrap_future(self.executor.submit(self.compute_all_channels, ts))
+                    for ts in self.stream.events
+                ])
+
+                averaged = np.mean(all_data, axis=0)  # shape: [n_channels][T]
+                for i, source in enumerate(self.sources):
+                    self.doc.add_next_tick_callback(partial(self.update_source, source, self.x_vals, averaged[i]))
+            else:
+                event_ts = int(self.event_type)
+                one_data = await asyncio.wrap_future(self.executor.submit(self.compute_all_channels, event_ts))
+
+                for i, source in enumerate(self.sources):
+                    self.doc.add_next_tick_callback(partial(self.update_source, source,self.x_vals, one_data[i]))
 
 
-def update_source(source, x_vals, y_vals):
-    source.data = {"x": x_vals, "y": y_vals}
+    def update_source(self, source, x_vals, y_vals):
+        source.data = {"x": x_vals, "y": y_vals}
 
-doc.add_periodic_callback(async_update_sources, 100)
-doc.title = "Live Ephys Data"
-
+ev = EventView(stream)
